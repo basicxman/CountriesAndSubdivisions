88c1315d41a35784ec894136456742838c003efd
Andrew Horsman <minirobotics@gmail.com>
2010-08-24 18:55:04 -0400

    Added bash line.

--- generator.rb
+++ generator.rb
@@ -1,5 +1,6 @@
-# Country and Subdivision SQL Generator
+#!/usr/bin/env ruby
 
+# @title: Country and Subdivision SQL Generator
 # @author: Andrew Horsman
 # @description: Generates a list of all subvision names for each country.
 
