95c419afbdbe6c1b625ca0b855cd7b9f6520c5bb
Andrew Horsman <minirobotics@gmail.com>
2010-08-24 18:53:56 -0400

    Added readme, re-downloaded proper file from CommonDataHub.  Iterated through each line in file and add it to a countries hash.

--- generator.rb
+++ generator.rb
@@ -2,3 +2,23 @@
 
 # @author: Andrew Horsman
 # @description: Generates a list of all subvision names for each country.
+
+# ./generator.rb file_to_convert.txt - make sure it exists.
+unless File.exists? ARGV[0]
+  puts "[-] File does not exist."
+  exit
+end
+
+countries = {} # Hash to store country => subdivisions[]
+
+File.open(ARGV[0]).each_line do |current_line|
+
+  values = current_line.split(';')
+  next unless values.length == 2
+  
+  countries[values[0]] == [] unless countries.includes? values[0] 
+  countries[values[0]] << values[1]
+
+end
+
+print countries
